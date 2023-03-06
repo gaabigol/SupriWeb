a815ebc03ce4671b178e40120844804807b138f2
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-03-06 22:37:31 +0000

    Project Created

---
+++ src/main/java/com/supriweb/supriweb/SupriwebApplication.java
@@ -0,0 +1,13 @@
+package com.supriweb.supriweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupriwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupriwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supriweb/supriweb/SupriwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supriweb.supriweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupriwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
